82021f6672fef550782b2dd7feb62735739f04cd
Andrey_Tarasov <lacryk@gmail.com>
2024-06-11 15:12:28 +0300

    обработка модуля processing линтерами

--- src/processing.py
+++ src/processing.py
@@ -1,7 +1,7 @@
-from typing import List, Dict
+from typing import Dict, List
 
 
-def filter_by_state(list_id: List[Dict], state: str = 'EXECUTED') -> List[Dict]:
+def filter_by_state(list_id: List[Dict], state: str = "EXECUTED") -> List[Dict]:
     """Функция сортировки списка по ключу "state"."""
     sorted_list_id = [dict_id for dict_id in list_id if dict_id["state"] == state]
     return sorted_list_id
