04ece8a5007790c0e9caec79f2d993a708b8db38
Andrey_Tarasov <lacryk@gmail.com>
2024-06-12 09:47:32 +0300

    мсправление файла processing.py

--- src/processing.py
+++ src/processing.py
@@ -7,7 +7,7 @@ def filter_by_state(list_id: List[Dict], state: str = "EXECUTED") -> List[Dict]:
     return sorted_list_id
 
 
-def sort_by_date(list_id: List[Dict], reverse: bool = False) -> List[Dict]:
+def sort_by_date(list_id: List[Dict], reverse: bool = True) -> List[Dict]:
     """Функция сортировки списка по дате (по возрастанию или убыванию)."""
     sorted_list = sorted(list_id, key=lambda dict_id: dict_id["date"], reverse=reverse)
     return sorted_list
