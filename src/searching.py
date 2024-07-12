e6b1fb21b1e049c44712c4c5dbadb4e0fde3b4df
Andrey_Tarasov <lacryk@gmail.com>
2024-07-12 15:09:14 +0300

    написание функций фильтрации словарей по строке поиска и по списку категорий

---
+++ src/searching.py
@@ -0,0 +1,48 @@
+import re
+from typing import List, Dict, Any
+from collections import defaultdict, Counter
+from src.utils import list_from_json
+
+
+def searching_description(list_transactions: List[Dict[str, Any]], user_str: str) -> List[Dict[str, Any]]:
+    """
+    Ищет банковские операции, в описании которых содержится заданная строка.
+    :param list_transactions: список словарей с данными о банковских операциях.
+    :param user_str: строка поиска.
+    :return: список словарей, у которых в описании есть данная строка.
+    """
+    pattern = re.compile(re.escape(user_str), re.IGNORECASE)
+
+    result = [transaction for transaction in list_transactions if pattern.search(transaction.get("description", ""))]
+
+    return result
+
+
+def searching_category(list_transactions: List[Dict[str, Any]], list_category: List[str]) -> Dict[str, int]:
+    """
+    Подсчитывает количество операций в каждой категории.
+    :param list_transactions: список словарей с данными о банковских операциях.
+    :param list_category: список категорий операций.
+    :return: словарь, где ключи — названия категорий, а значения — количество операций в каждой категории.
+    """
+
+    dict_category = defaultdict(int)
+
+    category_count = Counter()
+
+    for transaction in list_transactions:
+        description = str(transaction.get("description", ""))
+        for category in list_category:
+            if category in description:
+                category_count[category] += 1
+
+    return dict(category_count)
+
+
+if __name__ == "__main__":
+    # categories = ["Перевод с карты на карту", "Открытие вклада", "Перевод организации"]
+    # # categories = ["Перевод с карты на карту"]
+    # print(searching_category(list_from_json, categories))
+
+    user_string = "перевод"
+    print(searching_description(list_from_json, user_string))
--- src/utils.py
+++ src/utils.py
@@ -21,45 +21,38 @@ logging.basicConfig(
 transforming_operations_logger = logging.getLogger("utils.transforming_operations")
 
 
-def transforming_operations(filename):
+def transforming_operations():
     """
     Функцмя возвращает список транзакций в виде словарей, преобразованный из json-файла.
     """
-    if filename.suffix == ".json":
-        try:
-            transforming_operations_logger.info("запуск приложения")
-            with open(filename, encoding="utf-8") as file_json:
 
-                content = file_json.read().strip()
-                if not content:
-                    transforming_operations_logger.warning("Пустой список")
-                    return []
-                list_dicts = json.loads(content)
-                transforming_operations_logger.info("Преобразование списка в json формат")
+    filename = Path(ROOT_PATH, "operations.json")
+    try:
+        transforming_operations_logger.info("запуск приложения")
+        with open(filename, encoding="utf-8") as file_json:
 
-                if not isinstance(list_dicts, list):
-                    transforming_operations_logger.warning("На входе не список")
-                    return []
+            content = file_json.read().strip()
+            if not content:
+                transforming_operations_logger.warning("Пустой список")
+                return []
+            list_dicts = json.loads(content)
+            transforming_operations_logger.info("Преобразование списка в json формат")
 
-                return list_dicts
+            if not isinstance(list_dicts, list):
+                transforming_operations_logger.warning("На входе не список")
+                return []
 
-        except FileNotFoundError:
-            transforming_operations_logger.error("Файл не найден")
+            return list_dicts
 
-            return []
+    except FileNotFoundError:
+        transforming_operations_logger.error("Файл не найден")
 
-        except json.JSONDecodeError:
-            transforming_operations_logger.error("Некорректный формат json")
+        return []
 
-            return []
+    except json.JSONDecodeError:
+        transforming_operations_logger.error("Некорректный формат json")
 
-    elif filename.suffix == ".csv":
-
-        return from_csv()
-
-    elif filename.suffix == ".xlsx":
-
-        return from_xlsx()
+        return []
 
 
 def from_csv():
@@ -91,11 +84,16 @@ def from_xlsx():
 
 
 # Применение функции
-if __name__ == "__main__":
-    # operations_path = Path(ROOT_PATH, "operations.json")
-    operations_path = Path(ROOT_PATH, "transactions.csv")
-    # operations_path = Path(ROOT_PATH, "transactions_excel.xlsx")
+# if __name__ == "__main__":
 
-    returned_list = transforming_operations(operations_path)
-    # print(returned_list)
-    print(returned_list[3])
+# operations_path = Path(ROOT_PATH, "operations.json")
+# operations_path = Path(ROOT_PATH, "transactions.csv")
+# operations_path = Path(ROOT_PATH, "transactions_excel.xlsx")
+
+
+list_from_json = transforming_operations()
+list_from_csv = from_csv()
+list_from_excel = from_xlsx()
+
+# print(returned_list)
+# print(returned_list[3:10])
